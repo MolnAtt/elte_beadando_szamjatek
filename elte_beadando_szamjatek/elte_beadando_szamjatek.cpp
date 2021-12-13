30dd001e551a00b1196ad7d5a951403a5028bba7
Molnár Attila <molnar.attila@szlgbp.hu>
2021-12-13 18:31:09 +0100

    visszakövetést kivéve kész

--- elte_beadando_szamjatek/elte_beadando_szamjatek.cpp
+++ elte_beadando_szamjatek/elte_beadando_szamjatek.cpp
@@ -22,14 +22,20 @@ public:
 		return rek_haszon_mikor_jatekos_lep(0, N - 1);
 		/*/
 		haszonmatrixok_felepitese();
-		return Haszon_mikor_jatekos_lep[0][N - 1];
+		return Haszon_mikor_jatekos_lep->at(0)[N - 1];
 		/**/
 	}
 
+	string visszakovetes()
+	{
+		return "";
+	}
 
 	~Jatek()
 	{
 	}
+
+
 
 private:
 	int N;
@@ -38,19 +44,54 @@ private:
 	vector<vector<int>>* Haszon_mikor_ellenfel_lep;
 
 	/// <summary>
-/// "A játékos minden lépésben választhat egy számot a még az asztalon lévõ sorozatból, vagy a balszélsõt, vagy a jobbszélsõt, és leveszi a választott számot. [...] A játék akkor ér véget, ha elfogyott minden szám. A játékos nyereménye az általa választott számok összege.
-/// Írjon olyan programot, amely kiszámítja a játékos által elérhetõ maximális nyeremény értékét és meg is adja, hogy ezt milyen lépésekkel érheti el!"
-/// </summary>
-/// <param name="b">bal oldali szám indexe</param>
-/// <param name="j">jobb oldali szám indexe</param>
-/// <returns>a játékos haszna az ellenfél lépése után.</returns>
+	/// a két mátrix igazából két felsõháromszögmátrix, amelyben a jobb felsõ sarok értékét keressük. Ezt az alatta lévõ elemekbõl állítjuk elõ "szimultán rekurzióval", azaz a mátrixok egymásra is hivatkoznak a felépítés során. Lehetne egy (nemnégyzetes) mátrixba is tárolni a jobb helykihasználás végett, de nem akartam így elbonyolítani a kódott, jobb az átláthatóság.
+	/// </summary>
+	void haszonmatrixok_felepitese()
+	{
+		Haszon_mikor_jatekos_lep = new vector<vector<int>>(N, vector<int>(N));
+		Haszon_mikor_ellenfel_lep = new vector<vector<int>>(N, vector<int>(N));
+
+		// fõátló megalkotása:
+		for (int i = 0; i < N; i++)
+		{
+			Haszon_mikor_ellenfel_lep->at(i)[i] = 0;
+			Haszon_mikor_jatekos_lep->at(i)[i] = T->at(i);
+		}
+
+		// felsõháromszögek megalkotása:
+		// Ellenfél: 
+		for (int intervallumhossz = 1; intervallumhossz < N; intervallumhossz++)
+			for (int b = 0; b + intervallumhossz < N; b++)
+			{
+				int j = b + intervallumhossz;
+				Haszon_mikor_ellenfel_lep->at(b)[j] =
+					T->at(b) < T->at(j) ?
+					Haszon_mikor_jatekos_lep->at(b)[j - 1] :
+					Haszon_mikor_jatekos_lep->at(b + 1)[j];
+				Haszon_mikor_jatekos_lep->at(b)[j] = max(
+					T->at(j) + Haszon_mikor_ellenfel_lep->at(b)[j - 1],
+					T->at(b) + Haszon_mikor_ellenfel_lep->at(b + 1)[j]
+				);
+			}
+	}
+
+
+
+	/// <summary>
+	/// "A játékos minden lépésben választhat egy számot a még az asztalon lévõ sorozatból, vagy a balszélsõt, vagy a jobbszélsõt, és leveszi a választott számot. [...] A játék akkor ér véget, ha elfogyott minden szám. A játékos nyereménye az általa választott számok összege.
+	/// Írjon olyan programot, amely kiszámítja a játékos által elérhetõ maximális nyeremény értékét és meg is adja, hogy ezt milyen lépésekkel érheti el!"
+	/// </summary>
+	/// <param name="b">bal oldali szám indexe</param>
+	/// <param name="j">jobb oldali szám indexe</param>
+	/// <returns>a játékos haszna az ellenfél lépése után.</returns>
 	int rek_haszon_mikor_jatekos_lep(int b, int j)
 	{
 		if (b == j)
 			return T->at(b);
-
-		return max(T->at(j) + rek_haszon_mikor_ellenfel_lep(b, j - 1),
-			T->at(b) + rek_haszon_mikor_ellenfel_lep(b + 1, j));
+		return max(
+			T->at(j) + rek_haszon_mikor_ellenfel_lep(b, j - 1),
+			T->at(b) + rek_haszon_mikor_ellenfel_lep(b + 1, j)
+		);
 	}
 
 	/// <summary>
@@ -63,11 +104,9 @@ private:
 	{
 		if (b == j)
 			return 0;
-
-		if (T->at(b) < T->at(j))
-			return rek_haszon_mikor_jatekos_lep(b, j - 1);
-		else
-			return rek_haszon_mikor_jatekos_lep(b + 1, j);
+		return T->at(b) < T->at(j)?
+			rek_haszon_mikor_jatekos_lep(b, j - 1):
+			rek_haszon_mikor_jatekos_lep(b + 1, j);
 	}
 };
 
@@ -76,26 +115,34 @@ int main()
 {
 	Jatek J;
 	cout << J.maximalis_nyeremény();
+	cout << J.visszakovetes();
 
 }
 
 /* 
-=============================
-be0.txt:
+=========== be0.txt: ==================
+
+
 8
 5 12 3 21 33 14 6 2
+
 -----------------------------
 ki0.txt
+
 52
 JBJB
-=============================
-be1.txt: 
+
+=========== be1.txt: ==================
+
 20
 12 6 9 9 11 10 9 13 12 20 5 3 8 15 10 6 10 4 4 3
+
 -----------------------------
 ki1.txt
+
 100
 BBBJBBJJJJ
-=============================
+
+========================================
 
 */
